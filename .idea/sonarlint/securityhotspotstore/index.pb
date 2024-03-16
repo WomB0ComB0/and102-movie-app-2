68305ad5a96e25dfb0c53cdf51b2f79e8796f410
Mike Odnis <95197809+WomB0ComB0@users.noreply.github.com>
2024-03-16 15:35:19 -0400

    init commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+i
+9app/src/main/java/com/example/movie_app_2/MainActivity.kt,6\2\620db33563a88e81ead14d5de3ff825e61c8f61c
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+C
+settings.gradle.kts,7\4\749edfcc96398253e5b3416184e95c46621da850
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
---
+++ app/src/main/java/com/example/movie_app_2/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.movie_app_2
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import kotlinx.serialization.json.Json
+import okhttp3.Headers
+import org.json.JSONException
+
+fun createJson() = Json {
+    isLenient = true
+    ignoreUnknownKeys = true
+    useAlternativeNames = false
+}
+
+private const val TAG = "MainActivity/"
+private val SEARCH_API_KEY = BuildConfig.
+private const var API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
